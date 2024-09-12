bd42bb7b415c7c778d84e0b726d3d98912b3cd21
Daniel Adams <msub2official@gmail.com>
2024-09-12 09:50:31 -1000

    Update example to pull from page meta tags

--- index.js
+++ index.js
@@ -1,16 +1,27 @@
 async function signal() {
+  const url = document.head.querySelector('meta[data-canonical-url]')?.getAttribute('data-canonical-url');
+  const name = document.head.querySelector('meta[name="application-name"]')?.getAttribute('content');
+  const description = document.head.querySelector('meta[name="description"]')?.getAttribute('content');
+  if (!url || !name || !description) {
+    console.error("Missing required metadata! Check your <meta> tags for the following attributes: data-canonical-url, name=application-name, name=description");
+    return;
+  }
   const payload = {
-    url: document.location.href,
-    name: "beacon",
-    description: "test description",
+    url,
+    name,
+    description,
     active: true
   };
-  await fetch('http://localhost:8000/beacon', {
-    method: 'PUT',
-    body: JSON.stringify(payload),
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  });
+  // await fetch('https://relay-dev.zesty.xyz/beacon', {
+  //   method: 'PUT',
+  //   body: JSON.stringify(payload),
+  //   headers: {
+  //     'Content-Type': 'application/json'
+  //   }
+  // });
+  // await fetch('https://relay-dev2.zesty.xyz/test_follow', {
+  //   method: 'POST',
+  //   body: "relay@relay-dev.zesty.xyz",
+  // });
 }
 signal();
