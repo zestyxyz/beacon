674d561c0770ef87a7ee187f375b39dc4d122129
Daniel Adams <msub2official@gmail.com>
2024-09-12 16:51:39 -1000

    Expand beacon script to be a proper class

--- index.js
+++ index.js
@@ -1,27 +1,71 @@
-async function signal() {
-  const url = document.head.querySelector('meta[data-canonical-url]')?.getAttribute('data-canonical-url');
-  const name = document.head.querySelector('meta[name="application-name"]')?.getAttribute('content');
-  const description = document.head.querySelector('meta[name="description"]')?.getAttribute('content');
-  if (!url || !name || !description) {
-    console.error("Missing required metadata! Check your <meta> tags for the following attributes: data-canonical-url, name=application-name, name=description");
-    return;
+export default class Beacon {
+  relay;
+  specifiedName;
+  specifiedDescription;
+
+  constructor(relay, override = null) {
+    if (!relay) {
+      console.error("You must specify a relay URL for the beacon to connect to!");
+      return;
+    }
+    this.relay = relay;
+    if (override) {
+      this.specifiedName = override.name ?? null;
+      this.specifiedDescription = override.description ?? null;
+    }
   }
-  const payload = {
-    url,
-    name,
-    description,
-    active: true
-  };
-  // await fetch('https://relay-dev.zesty.xyz/beacon', {
-  //   method: 'PUT',
-  //   body: JSON.stringify(payload),
-  //   headers: {
-  //     'Content-Type': 'application/json'
-  //   }
-  // });
-  // await fetch('https://relay-dev2.zesty.xyz/test_follow', {
-  //   method: 'POST',
-  //   body: "relay@relay-dev.zesty.xyz",
-  // });
+
+  getUrl() {
+    const meta = document.head.querySelector('meta[data-canonical-url]');
+    if (meta) {
+      return meta.getAttribute('data-canonical-url');
+    } else {
+      return document.location.href;
+    }
+  }
+
+  getName() {
+    if (this.specifiedName) return this.specifiedName;
+
+    const meta = document.head.querySelector('meta[name="application-name"]');
+    if (meta) {
+      return meta.getAttribute('content');
+    } else {
+      return document.title;
+    }
+  }
+
+  getDescription() {
+    if (this.specifiedDescription) return this.specifiedDescription;
+
+    const meta = document.head.querySelector('meta[name="description"]');
+    if (meta) {
+      return meta.getAttribute('content');
+    } else {
+      return "";
+    }
+  }
+
+  async signal() {
+    const url = this.getUrl();
+    const name = this.getName();
+    const description = this.getDescription();
+    if (!url || !name || !description) {
+      console.error("Missing required metadata! Check your <meta> tags for the following attributes: data-canonical-url, name=application-name, name=description");
+      return;
+    }
+    const payload = {
+      url,
+      name,
+      description,
+      active: true
+    };
+    await fetch(`${this.relay}/beacon`, {
+      method: 'PUT',
+      body: JSON.stringify(payload),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  }
 }
-signal();
